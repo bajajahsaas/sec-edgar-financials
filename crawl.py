fcd977218568d7c72458d604785489307f925746
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-15 00:17:42 -0500

    Add try except

--- crawl.py
+++ crawl.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[23]:
+
+
 import json
 import numpy as np
 import pandas as pd
@@ -9,7 +15,7 @@ https://www.pingshiuanchua.com/blog/post/overpower-your-google-sheets-with-pytho
 
 '''
 
-def getData(ticket, type, period='annual', year=20188, quarter=0):
+def getData(ticket, ty, period='annual', year=2018, quarter=0):
     # period = 'annual' # or 'quarterly', which is the default
     # year = 2018 # can use default of 0 to get the latest
     # quarter = 1 # 1, 2, 3, 4, or default value of 0 to get the latest
@@ -17,51 +23,53 @@ def getData(ticket, type, period='annual', year=20188, quarter=0):
     stock = Stock(ticket)
     try:
         filing = stock.get_filing(period, year, quarter)
+        # financial reports (contain data for multiple years)
+        if ty == 'income_statements':
+            statements = filing.get_income_statements()
+        elif ty == "balance_sheets":
+            statements = filing.get_balance_sheets()
+        elif ty == "cash_flows":
+            statements = filing.get_cash_flows()
+
+        jsonstr = FinancialReportEncoder().encode(statements)
+        data = json.loads(jsonstr)
+
+        # print(data.keys())  # dict_keys(['company', 'date_filed', 'reports'])
+        listreports = data['reports']
+        columns = []
+        data = []
+
+        map_keys = []
+
+        for report in listreports:
+            # print(report.keys())  # dict_keys(['date', 'months', 'map']): string, int, dict
+
+            for report_key in report.keys():
+                if report_key == 'map':
+                    for map_key in report['map'].keys():
+                        map_keys.append(map_key)
+                        # print(map_key, report['map'][map_key].keys())  # dict_keys(['label', 'value'])
+                        for key in report['map'][map_key].keys():
+                            # print(key, report['map'][map_key][key])
+                            if key == 'label':
+                                columns.append(report['map'][map_key][key])
+                            else:
+                                data.append(report['map'][map_key][key])
+
+        sec_data = pd.DataFrame([data], columns=columns)
+        sec_data = sec_data.transpose()
+        sec_data.reset_index(inplace=True)
+        sec_data['ind'] = np.arange(len(sec_data))
+        sec_data.set_index("ind", inplace=True)
+        map_data = pd.Series(map_keys)
+        return sec_data, True
+
     except:
-        print('Invalid input')
+        print('Invalid company input', cmp, ty, period, year, quarter)
         return None, False
 
-    # financial reports (contain data for multiple years)
-    if type == 'income_statements':
-        statements = filing.get_income_statements()
-    elif type == "balance_sheets":
-        statements = filing.get_balance_sheets()
-    elif type == "cash_flows":
-        statements = filing.get_cash_flows()
 
-    jsonstr = FinancialReportEncoder().encode(statements)
-    data = json.loads(jsonstr)
-
-    # print(data.keys())  # dict_keys(['company', 'date_filed', 'reports'])
-
-    listreports = data['reports']
-
-    columns = []
-    data = []
-
-    map_keys = []
-
-    for report in listreports:
-        # print(report.keys())  # dict_keys(['date', 'months', 'map']): string, int, dict
-
-        for report_key in report.keys():
-            if report_key == 'map':
-                for map_key in report['map'].keys():
-                    map_keys.append(map_key)
-                    # print(map_key, report['map'][map_key].keys())  # dict_keys(['label', 'value'])
-                    for key in report['map'][map_key].keys():
-                        # print(key, report['map'][map_key][key])
-                        if key == 'label':
-                            columns.append(report['map'][map_key][key])
-                        else:
-                            data.append(report['map'][map_key][key])
-
-    sec_data = pd.DataFrame([data], columns=columns)
-    sec_data = sec_data.append(pd.Series(), ignore_index=True)
-    sec_data = sec_data.transpose()
-
-    map_data = pd.Series(map_keys)
-    return sec_data, True
+# In[24]:
 
 
 final_df = pd.DataFrame()
@@ -74,28 +82,29 @@ cmpList = ['AAL', 'AAPL', 'ADBE', 'ADI', 'ADP', 'ADSK', 'ALGN', 'ALXN', 'AMAT', 
            'SIRI', 'SNPS', 'STX', 'SWKS', 'SYMC', 'TMUS', 'TSLA', 'TTWO', 'TXN', 'ULTA', 'VOD', 'VRSK', 'VRTX', 'WBA',
            'WDAY', 'WDC', 'WYNN', 'XLNX', 'XRAY']
 
-cmpList = ['AAL']
-
 print('Total companies', len(cmpList))
 
 types = ['income_statements', 'balance_sheets', 'cash_flows']
 years = [2019, 2018, 2017, 2016]
+writer = pd.ExcelWriter('results/results.xlsx', engine='xlsxwriter')
 
 for cmp in cmpList:
+    columns = []
     cmp_df = pd.DataFrame()
     cmp_df['ind'] = np.arange(len(cmp_df))
     cmp_df.set_index("ind", inplace=True)
-    for type in types:
-        writer = pd.ExcelWriter('results/results.xlsx', engine='xlsxwriter')
+    for ty in types:
         for yr in years:
-            typedf, valid = getData(cmp, type, year = yr)
+            print('Company: ', cmp)
+            typedf, valid = getData(cmp, ty, year = yr)
             if valid:
-                typedf['ind'] = np.arange(len(typedf))
-                typedf.set_index("ind", inplace=True)
-                print('concat', cmp_df.shape, typedf.shape)
-                # cmp_df = pd.concat([cmp_df,typedf], ignore_index=True, axis = 1)
-                cmp_df.merge(typedf, how="outer", left_index=True, right_index=True)
-                print('concat done', cmp_df.shape)
-    cmp_df.to_excel(writer, sheet_name=cmp)
-    writer.save()
-    writer.close()
+                cmp_df = pd.concat([cmp_df,typedf], axis = 1)
+                columns.append(str(ty) + '_' + str(yr))
+                columns.append(str(ty) + '_' + str(yr))
+    
+    cmp_df.columns = columns
+    cmp_df.to_excel(writer, sheet_name=cmp, index = False)
+
+writer.save()
+writer.close()
+
