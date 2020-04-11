b5d07cde9b6b3ecf011b762f8bff7b9bd6b2fa7e
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-04-10 21:15:48 -0400

    Optimize code

--- crawl.py
+++ crawl.py
@@ -39,29 +39,28 @@ def getData(ticket, ty, period='annual', year=2018, quarter=0):
         columns = []
         data = []
 
-        map_keys = []
-
+        print(type(listreports), len(listreports))
         for report in listreports:
+            # listreports is a list of 3 dictionaries (all have same labels with different values)
             # print(report.keys())  # dict_keys(['date', 'months', 'map']): string, int, dict
 
-            for report_key in report.keys():
-                if report_key == 'map':
-                    for map_key in report['map'].keys():
-                        map_keys.append(map_key)
-                        # print(map_key, report['map'][map_key].keys())  # dict_keys(['label', 'value'])
-                        for key in report['map'][map_key].keys():
-                            # print(key, report['map'][map_key][key])
-                            if key == 'label':
-                                columns.append(report['map'][map_key][key])
-                            else:
-                                data.append(report['map'][map_key][key])
+            report = report['map']
+            for dict in report.values():
+                # print(map_key, report['map'][map_key].keys())  # dict_keys(['label', 'value'])
+                label = dict['label']
+                columns.append(label)
+                # print('Label', label)
+
+                value = dict['value']
+                data.append(value)
+                # print('Value', value)
+            break
 
         sec_data = pd.DataFrame([data], columns=columns)
         sec_data = sec_data.transpose()
         sec_data.reset_index(inplace=True)
         sec_data['ind'] = np.arange(len(sec_data))
         sec_data.set_index("ind", inplace=True)
-        map_data = pd.Series(map_keys)
         return sec_data, True
 
     except:
@@ -73,19 +72,25 @@ def getData(ticket, ty, period='annual', year=2018, quarter=0):
 
 
 final_df = pd.DataFrame()
-cmpList = ['AAL', 'AAPL', 'ADBE', 'ADI', 'ADP', 'ADSK', 'ALGN', 'ALXN', 'AMAT', 'AMGN', 'AMZN', 'ASML', 'ATVI', 'AVGO',
-           'BIDU', 'BIIB', 'BKNG', 'BMRN', 'CA', 'CDNS', 'CELG', 'CERN', 'CHKP', 'CHTR', 'CMCSA', 'COST', 'CSCO', 'CSX',
-           'CTAS', 'CTRP', 'CTSH', 'CTXS', 'DISH', 'DLTR', 'EA', 'EBAY', 'ESRX', 'EXPE', 'FAST', 'FB', 'FISV', 'FOX',
-           'FOXA', 'GILD', 'GOOG', 'HAS', 'HOLX', 'HSIC', 'IDXX', 'ILMN', 'INCY', 'INTC', 'INTU', 'ISRG', 'JBHT', 'JD',
-           'KHC', 'KLAC', 'LBTYA', 'LBTYK', 'LRCX', 'MAR', 'MCHP', 'MDLZ', 'MELI', 'MNST', 'MSFT', 'MU', 'MXIM', 'MYL',
-           'NFLX', 'NTES', 'NVDA', 'ORLY', 'PAYX', 'PCAR', 'PYPL', 'QCOM', 'QRTEA', 'REGN', 'ROST', 'SBUX', 'SHPG',
-           'SIRI', 'SNPS', 'STX', 'SWKS', 'SYMC', 'TMUS', 'TSLA', 'TTWO', 'TXN', 'ULTA', 'VOD', 'VRSK', 'VRTX', 'WBA',
-           'WDAY', 'WDC', 'WYNN', 'XLNX', 'XRAY']
+# cmpList = ['AAL', 'AAPL', 'ADBE', 'ADI', 'ADP', 'ADSK', 'ALGN', 'ALXN', 'AMAT', 'AMGN', 'AMZN', 'ASML', 'ATVI', 'AVGO',
+#            'BIDU', 'BIIB', 'BKNG', 'BMRN', 'CA', 'CDNS', 'CELG', 'CERN', 'CHKP', 'CHTR', 'CMCSA', 'COST', 'CSCO', 'CSX',
+#            'CTAS', 'CTRP', 'CTSH', 'CTXS', 'DISH', 'DLTR', 'EA', 'EBAY', 'ESRX', 'EXPE', 'FAST', 'FB', 'FISV', 'FOX',
+#            'FOXA', 'GILD', 'GOOG', 'HAS', 'HOLX', 'HSIC', 'IDXX', 'ILMN', 'INCY', 'INTC', 'INTU', 'ISRG', 'JBHT', 'JD',
+#            'KHC', 'KLAC', 'LBTYA', 'LBTYK', 'LRCX', 'MAR', 'MCHP', 'MDLZ', 'MELI', 'MNST', 'MSFT', 'MU', 'MXIM', 'MYL',
+#            'NFLX', 'NTES', 'NVDA', 'ORLY', 'PAYX', 'PCAR', 'PYPL', 'QCOM', 'QRTEA', 'REGN', 'ROST', 'SBUX', 'SHPG',
+#            'SIRI', 'SNPS', 'STX', 'SWKS', 'SYMC', 'TMUS', 'TSLA', 'TTWO', 'TXN', 'ULTA', 'VOD', 'VRSK', 'VRTX', 'WBA',
+#            'WDAY', 'WDC', 'WYNN', 'XLNX', 'XRAY']
+
+cmpList = ['AAPL']
 
 print('Total companies', len(cmpList))
 
-types = ['income_statements', 'balance_sheets', 'cash_flows']
-years = [2019, 2018, 2017, 2016]
+# types = ['income_statements', 'balance_sheets', 'cash_flows']
+# years = [2019, 2018, 2017, 2016]
+
+types = ['income_statements']
+years = [2019]
+
 writer = pd.ExcelWriter('results/results.xlsx', engine='xlsxwriter')
 
 for cmp in cmpList:
